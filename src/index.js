671f8ec4c60aaa32e73b80babff606f67efa9a11
Jonathan Bursztyn <jonathan.bursztyn@mulesoft.com>
2017-11-21 13:27:58 -0300

    Moved docs

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './Router';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
