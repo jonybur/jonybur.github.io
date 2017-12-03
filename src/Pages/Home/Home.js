c4e8c6d827ce92cd9ac9a13f711a18a6538ade11
Jonathan Bursztyn <jonathan.bursztyn@mulesoft.com>
2017-12-03 13:29:46 -0300

    Took out unfinished stuf

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -16,20 +16,20 @@ export class Home extends PureComponent {
             <h1>Hey there,</h1>
             <p>My name is Jonathan Bursztyn,</p>
             <p>I'm a Software Engineer at <a href="https://www.mulesoft.com" target="_blank">MuleSoft</a>.</p>
-            <p>During my free time I enjoy <Link to="/photography">photography</Link>.</p>
+            {/*<p>During my free time I enjoy <Link to="/photography">photography</Link>.</p>*/}
           </div>
           <div className="links">
-            <Link to="/portfolio">Portfolio</Link>
+            {/*<Link to="/portfolio">Portfolio</Link>*/}
             <a href="https://www.github.com/jonybur" target="_blank">GitHub</a>
             <a href="https://www.linkedin.com/in/jonybur" target="_blank">LinkedIn</a>
             <a href="mailto:jonathan@bursz.com" target="_blank">Contact</a>
           </div>
-          <img className="mac" src="mac.png" alt="Mountain View"></img>
+          {/*<img className="mac" src="mac.png" alt="Mountain View"></img>
           <YouTube
             video="W9sxKjq44AA"
             autoplay={true}
             className="video"
-          />
+          />*/}
         </div>
       </div>
     );
