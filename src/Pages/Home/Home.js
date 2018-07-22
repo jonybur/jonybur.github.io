229198b85a3510ac868a5fd57fc9349b68da84db
Jonathan Bursztyn <jonathan.bursztyn@mulesoft.com>
2018-07-22 16:49:35 -0300

    Added colors to menu

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -17,12 +17,19 @@ export class Home extends PureComponent {
     return (
       <div className="wrapper">
         <div className="links">
-          <a href="" className="links-portfolio">portfolio</a>
-          <a href="" className="links-linkedin">linkedin</a>
-          <a href="" className="links-github">github</a>
-          <a href="" className="links-photography">photography</a>
+          <div className="links-1">
+            <a href="" className="links-portfolio">portfolio</a>
+          </div>
+          <div className="links-2">
+            <a href="" className="links-linkedin">linkedin</a>
+          </div>
+          <div className="links-3">
+            <a href="" className="links-github">github</a>
+          </div>
+          <div className="links-4">
+            <a href="" className="links-photography">photography</a>
+          </div>
         </div>
-
         <div className="content-wrapper">
           <img className="content" src={"photo.jpg"} alt="Vintage Macintosh"/>
         </div>
