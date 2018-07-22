9c57bfb0e70828fdc242d66bb785c49619503ff6
Jonathan Bursztyn <jonathan.bursztyn@mulesoft.com>
2018-07-22 19:15:59 -0300

    Working on site

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,26 +12,29 @@ export class Home extends PureComponent {
     super(props);
   }
 
-
   render() {
     return (
       <div className="wrapper">
-        <div className="links">
-          <div className="links-1">
-            <a href="" className="links-portfolio">portfolio</a>
+        <div className="leftColumn">
+          <span className="initials">jb</span>
+          <div className="links"> 
+              <div className="links-portfolio-wrapper">
+                <a href="" className="links-portfolio">portfolio</a>
+              </div>
+              <div className="links-linkedin-wrapper">
+                <a href="" className="links-linkedin">linkedin</a>
+              </div>
+              <div className="links-github-wrapper">
+                <a href="" className="links-github">github</a>
+              </div>
+              <div className="links-photography-wrapper">
+                <a href="" className="links-photography">photography</a>
+              </div>
           </div>
-          <div className="links-2">
-            <a href="" className="links-linkedin">linkedin</a>
-          </div>
-          <div className="links-3">
-            <a href="" className="links-github">github</a>
-          </div>
-          <div className="links-4">
-            <a href="" className="links-photography">photography</a>
-          </div>
+          <div className="footer">a</div>
         </div>
-        <div className="content-wrapper">
-          <img className="content" src={"photo.jpg"} alt="Vintage Macintosh"/>
+        <div className="photo-wrapper">
+          <img className="photo" src={"photo.jpg"} alt="Vintage Macintosh"/>
         </div>
       </div>
     );
