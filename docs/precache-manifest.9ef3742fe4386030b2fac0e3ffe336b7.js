29eace4a1834349bef886b8e1d8a86747c84dc6c
Jonathan Bursztyn <jobur93@gmail.com>
2019-07-05 22:14:48 -0300

    Update site

---
+++ docs/precache-manifest.9ef3742fe4386030b2fac0e3ffe336b7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "d639a65a2b565960de0288d8880cb17b",
+    "url": "/static/media/7.d639a65a.png"
+  },
+  {
+    "revision": "017238d379e435d14e65",
+    "url": "/static/css/main.021cc51a.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "790c957530de6da94eec",
+    "url": "/static/js/2.9a0d1d21.chunk.js"
+  },
+  {
+    "revision": "e71bfc03c44b70f9a7f0f14ddcc3176b",
+    "url": "/static/media/1.e71bfc03.png"
+  },
+  {
+    "revision": "f2e75aaffdcbb88aaacd6e3816b0e420",
+    "url": "/static/media/1.f2e75aaf.png"
+  },
+  {
+    "revision": "3652466d9104429ffcf62b1efb803761",
+    "url": "/static/media/8.3652466d.png"
+  },
+  {
+    "revision": "d7b20a7337e60af5ed0adc5332481341",
+    "url": "/static/media/1.d7b20a73.png"
+  },
+  {
+    "revision": "effe45ed1fb1ccfdd5125bf318bb487d",
+    "url": "/static/media/2.effe45ed.png"
+  },
+  {
+    "revision": "d38cd0fb05831bc21658025251ae8d76",
+    "url": "/static/media/5.d38cd0fb.png"
+  },
+  {
+    "revision": "8b0fd253238373cc03f3bacaba65e8af",
+    "url": "/static/media/4.8b0fd253.png"
+  },
+  {
+    "revision": "017238d379e435d14e65",
+    "url": "/static/js/main.094ab57e.chunk.js"
+  },
+  {
+    "revision": "16efece557d9af664d3db5ce1fc3f1ab",
+    "url": "/static/media/1.16efece5.png"
+  },
+  {
+    "revision": "aa1f581f812b68c402a8d78c368af138",
+    "url": "/static/media/empty_iphone.aa1f581f.png"
+  },
+  {
+    "revision": "44a7486ef44d3cfb116273e30ee4a5a6",
+    "url": "/static/media/bais_screen1.44a7486e.png"
+  },
+  {
+    "revision": "846f2eb324063217ea6c9776ca999080",
+    "url": "/static/media/bais_screen2.846f2eb3.png"
+  },
+  {
+    "revision": "d15b989b6ea097929ef1b690e3335277",
+    "url": "/static/media/bais_screen3.d15b989b.png"
+  },
+  {
+    "revision": "9cae1e2cef560b3c8d037b1b32b361dc",
+    "url": "/static/media/cyberspace.9cae1e2c.otf"
+  },
+  {
+    "revision": "07aeebd3f71f77a0f7b3b4fbe09cce8e",
+    "url": "/static/media/NeutraText-DemiSC.07aeebd3.otf"
+  },
+  {
+    "revision": "591c1e12b842f438fb20429634ea9626",
+    "url": "/static/media/NeutraText-Demi.591c1e12.otf"
+  },
+  {
+    "revision": "cf9277dcadd1c65874736d760ac8415a",
+    "url": "/static/media/NeutraText-Book.cf9277dc.otf"
+  },
+  {
+    "revision": "17221a21fa9f650aa16ed08d72c37778",
+    "url": "/static/media/NeutraText-BookItalic.17221a21.otf"
+  },
+  {
+    "revision": "fcabf7c3566102baf5c4f622c2bd3fb5",
+    "url": "/index.html"
+  }
+];
